ede99d52627e43d1839ffd783978cb40f07287a1
davi-ufop <davi.ufop@gmail.com>
2022-01-19 22:15:00 -0300

    paper01AU

--- artigos/paper01/myrobot/mylib06.py
+++ artigos/paper01/myrobot/mylib06.py
@@ -3,18 +3,21 @@
 
 ###### Bibliotecas e módulos
 import numpy as np
-import pylab as pl
 from scipy.integrate import odeint
-import pykoopman as kp
+from tqdm import tqdm
 
-### Condições iniciais
-t = np.arange(0, 30, 0.01)
-x0 = np.array([-0.8, 0, -0.5, 0])  
+### Semente randômica
+np.random.seed(888)
+### Número de simulações
+N = 50      
+t = np.arange(0, 20, 0.01)
+
+###### SISTEMA: PÊNDULO DUPLO
+### Parâmetros 
 l1, l2, m1, m2 = 1, 1, 1, 1
 g = 9.82
-
-### Sistema de EDOs do pêndulo duplo: Vide Wikipédia
-def sistema(x, t):
+### Sistema de EDOs do pêndulo duplo
+def dpendulo(x, t):
   ### Entrada de dados
   q1, q2, p1, p2 = x
   ### Sistema de EDOs acopladas - Vide Referência
@@ -25,46 +28,78 @@ def sistema(x, t):
   ### Retorno do sistema
   return dq1_dt, dq2_dt, dp1_dt, dp2_dt
 
-### Integrando o sistema de EDOs
-x = odeint(sistema, x0, t)
+###### RESOLUÇÃO DO PÊNDULO DUPLO
+### Resolução
+print("\nPêndulo duplo:")
+for i in tqdm(range(N)):
+  ### Condições iniciais
+  teta1 = np.random.uniform(-np.pi/3, np.pi/3)
+  teta2 = np.random.uniform(-np.pi/2, np.pi/2)
+  x0 = np.array([teta1, 0, teta2, 0]) 
+  ### Integrando o sistema de EDOs
+  x = odeint(dpendulo, x0, t)
+  ### Salvando as informações
+  DIR = "prog07/data/dpendulo/"
+  np.savetxt(DIR+"entrada{:02d}.csv".format(i+1), x0, delimiter=",")
+  np.savetxt(DIR+"saida{:02d}.csv".format(i+1), x, delimiter=",")
 
-### Ajustando a solução
-teta1, teta2, omega1, omega2 = x[:,0], x[:,1], x[:,2], x[:,3]
-xk = l1*np.sin(teta1) + l2*np.sin(teta2)            ## Posições cartesianas da ponta do pêndulo
-yk = l1*np.cos(teta1) + l2*np.cos(teta2)
 
-pl.plot(xk, yk, 'b-')
-pl.xlabel('x')
-pl.ylabel('y')
-pl.show()
-pl.clf()
+###### SISTEMA DE DUFFING
+### Sistema de EDOS de Duffing (Pêndulo Simples)
+def duffing(x, t):
+  ## Entrada das variáveis
+  x1, x2 = x
+  ## Equações diferenciais acopladas -> Pêndulo Simples O(3)
+  dx1_dt = x2
+  dx2_dt = x1 - x1**3
+  ## Retorno do sistema
+  return dx1_dt, dx2_dt
 
-Field = np.array([xk, yk]).T
+###### RESOLUÇÃO DO SISTEMA DE DUFFING
+### Resolução
+print("\nPêndulo simples:")
+for i in tqdm(range(N)):
+  ### Condições iniciais
+  teta1 = np.random.uniform(-np.pi/3, np.pi/3)
+  omega1 = np.random.uniform(0, 0.5)
+  x0 = np.array([teta1, omega1])
+  ### Integrando o sistema de EDOs
+  x = odeint(duffing, x0, t)
+  ### Salvando as informações
+  DIR = "prog07/data/duffing/"
+  np.savetxt(DIR+"entrada{:02d}.csv".format(i+1), x0, delimiter=",")
+  np.savetxt(DIR+"saida{:02d}.csv".format(i+1), x, delimiter=",")
 
-modelo = kp.Koopman()
-modelo.fit(Field)
 
-K = modelo.koopman_matrix.real
-print(K)
-#pl.matshow(K)
-#pl.show()
-#pl.clf()
+######  SISTEMA DE LORENZ
+### Sistema de Lorenz
+def lorenz(x, t, S, B, R):
+  ## Condição
+  x1, x2, x3 = x
+  ## Sistema: Taxa de convecção (1) + 
+  ## Temperaturas horizontais (2) e verticais (3)
+  dx1_dt = S*(x2 - x1)
+  dx2_dt = x1*(R - x3) - x2
+  dx3_dt = x1*x2 - B*x3
+  ## Resultado
+  return dx1_dt, dx2_dt, dx3_dt
 
-Fprev = np.vstack((Field[0], modelo.simulate(Field[0], n_steps=Field.shape[0]-1)))
-
-xp = Fprev[:,0].real
-yp = Fprev[:,1].real
-
-pl.plot(xp, yp, 'r-')
-pl.xlabel('x')
-pl.ylabel('y')
-pl.show()
-pl.clf()
-
-dy = abs(yp[1:50] -yk[1:50])
-
-pl.plot(dy, 'k.')
-pl.show()
-pl.clf()
+###### RESOLUÇÃO DO SISTEMA DE LORENZ
+### Parâmetros
+sigma, beta, rho = 10, 8/3, 28
+### Resolução
+print("\nSistema atmosférico:")
+for i in tqdm(range(50)):
+  ### Condições iniciais
+  x01 = np.random.uniform(0.8, 2.3)
+  x02 = np.random.uniform(0.2, 1.1)
+  x03 = np.random.uniform(1.3, 3.2)
+  x0 = np.array([x01, x02, x03])
+  ### Integrando o sistema de ODEs
+  x = odeint(lorenz, x0, t, args=(sigma, beta, rho))
+  ### Salvando as informações
+  DIR = "prog07/data/lorenz/"
+  np.savetxt(DIR+"entrada{:02d}.csv".format(i+1), x0, delimiter=",")
+  np.savetxt(DIR+"saida{:02d}.csv".format(i+1), x, delimiter=",")
 
 ### FIM 
--- artigos/paper01/myrobot/prog07_koopman.py
+++ artigos/paper01/myrobot/prog07_koopman.py
@@ -1,2 +1,266 @@
-### Programa que o robô vai usar RL com Koopman para aprender a pegar três objetos
-### Use seed
+### Programa que utiliza o operador de Koopman para reconstruir
+### as trajetórias no espaço de fases 
+### Davi Neves - Ouro Preto, Brasil - Jan. 2022
+
+### Bibliotecas e módulos
+import numpy as np          ### Numérica
+import pylab as pl          ### Gráfica
+import pykoopman as kp      ### Método de Koopman
+from tqdm import tqdm       ### Barras de progresso
+from imageio import imread  ### Imagens
+
+### Diretórios
+DDP = "prog07/data/dpendulo/"
+DDS = "prog07/data/duffing/"
+DDL = "prog07/data/lorenz/"
+DIP = "prog07/imgs/dpendulo/"
+DIS = "prog07/imgs/duffing/"
+DIL = "prog07/imgs/lorenz/"
+DKP = "prog07/koopman/dpendulo/"
+DKS = "prog07/koopman/duffing/"
+DKL = "prog07/koopman/lorenz/"
+
+### Parâmetros de mylib06,py
+N = 50
+
+###### PÊNDULO DUPLO
+### Importando dados
+print("\nPêndulo duplo:")
+for i in tqdm(range(N)):
+  ### Condições iniciais
+  x0 = np.genfromtxt(DDP+"entrada{:02d}.csv".format(i+1), delimiter=",")
+  ### Trajetórias -> Dados: Data-Drive
+  x = np.genfromtxt(DDP+"saida{:02d}.csv".format(i+1), delimiter=",")
+  teta1, teta2, omega1, omega2 = x[:,0], x[:,1], x[:,2], x[:,3]
+  ### Plotando o espaço de fases 1
+  pl.plot(teta1, omega1, 'b.')
+  pl.title(str(list(np.round(x0,2))))
+  pl.xlabel('teta1')
+  pl.ylabel('omega1')
+  pl.savefig(DIP+"efases1{:02d}.png".format(i+1), dpi=200)
+  pl.clf()
+  ### Plotando o espaço de fases 2
+  pl.plot(teta2, omega2, 'b-')
+  pl.title(str(list(np.round(x0,2))))
+  pl.xlabel('teta2')
+  pl.ylabel('omega2')
+  pl.savefig(DIP+"efases2{:02d}.png".format(i+1), dpi=200)
+  pl.clf()
+  ###### OPERADOR DE KOOPMAN
+  ### Adequando os dados para usarmos no modelo -> Vide [1]
+  Field = np.array([teta1, teta2, omega1, omega2]).T
+  ### Determinando o operador (matriz) de Koopman
+  modelo = kp.Koopman()             ### Modelo do método
+  modelo.fit(Field)                 ### Ajustando os dados
+  K = modelo.koopman_matrix.real    ### Determinando a matrix
+  ### Salvando o operador
+  np.savetxt(DKP+"koopman{:02d}.csv".format(i+1), K, delimiter=",")
+  pl.matshow(K)
+  pl.savefig(DIP+"operador{:02d}.png".format(i+1), dpi=200)
+  pl.clf()
+  ### Fazendo a reconstrução (rebuild) das trajetórias
+  Frb = np.vstack((Field[0], modelo.simulate(Field[0], n_steps=Field.shape[0]-1)))
+  x1 = Frb[:,0].real
+  x2 = Frb[:,1].real
+  x3 = Frb[:,2].real
+  x4 = Frb[:,3].real
+  ### Espaço de fases 1 reconstruido
+  pl.plot(x1, x3, 'r.')
+  pl.xlabel('teta1')
+  pl.ylabel('omega1')
+  pl.savefig(DIP+"koopman1{:02d}.png".format(i+1), dpi=200)
+  pl.clf()
+  ### Espaço de fases 2 reconstruido
+  pl.plot(x2, x4, 'r-')
+  pl.xlabel('teta2')
+  pl.ylabel('omega2')
+  pl.savefig(DIP+"koopman2{:02d}.png".format(i+1), dpi=200)
+  pl.clf()
+  
+###### Combinando imagens
+num = 32
+pl.figure(figsize=(8, 8), dpi=128)
+imagem1 = imread(DIP+"efases1{:02d}.png".format(num))
+imagem2 = imread(DIP+"efases2{:02d}.png".format(num))
+imagem3 = imread(DIP+"koopman1{:02d}.png".format(num))
+imagem4 = imread(DIP+"koopman2{:02d}.png".format(num))
+### Espaço de fases 1
+pl.subplot(2, 2, 1)
+pl.axis('off')
+pl.imshow(imagem1)
+pl.title('(A)', y=-0.1)
+### Espaço de fases 2
+pl.subplot(2, 2, 2)
+pl.axis('off')
+pl.imshow(imagem2)
+pl.title('(B)', y=-0.1)
+### Espaço de fases reconstruido 1
+pl.subplot(2, 2, 3)
+pl.axis('off')
+pl.imshow(imagem3)
+pl.title('(C)', y=-0.1)
+### Espaço de fases reconstruido 2
+pl.subplot(2, 2, 4)
+pl.axis('off')
+pl.imshow(imagem4)
+pl.title('(D)', y=-0.1)
+### Salvando
+pl.savefig("prog07/montagem_dpendulo.png")
+pl.clf()
+
+###### SISTEMA DE LORENZ
+### Importando dados
+print("\nLorenz:")
+for i in tqdm(range(N)):
+  ### Condições iniciais
+  x0 = np.genfromtxt(DDL+"entrada{:02d}.csv".format(i+1), delimiter=",")
+  ### Trajetórias -> Dados: Data-Drive
+  x = np.genfromtxt(DDL+"saida{:02d}.csv".format(i+1), delimiter=",")
+  C1, T1, T2 = x[:,0], x[:,1], x[:,2]
+  ### Plotando o espaço de fases 1
+  pl.plot(C1, T1, 'b.')
+  pl.title(str(list(np.round(x0,2))))
+  pl.xlabel('convecção')
+  pl.ylabel('temperatura horizontal')
+  pl.savefig(DIL+"efases1{:02d}.png".format(i+1), dpi=200)
+  pl.clf()
+  ### Plotando o espaço de fases 2
+  pl.plot(C1, T2, 'b-')
+  pl.title(str(list(np.round(x0,2))))
+  pl.xlabel('convecção')
+  pl.ylabel('temperatura vertical')
+  pl.savefig(DIL+"efases2{:02d}.png".format(i+1), dpi=200)
+  pl.clf()
+  ###### OPERADOR DE KOOPMAN
+  ### Adequando os dados para usarmos no modelo -> Vide [1]
+  Field = np.array([C1, T1, T2]).T
+  ### Determinando o operador (matriz) de Koopman
+  modelo = kp.Koopman()             ### Modelo do método
+  modelo.fit(Field)                 ### Ajustando os dados
+  K = modelo.koopman_matrix.real    ### Determinando a matrix
+  ### Salvando o operador
+  np.savetxt(DKL+"koopman{:02d}.csv".format(i+1), K, delimiter=",")
+  pl.matshow(K)
+  pl.savefig(DIL+"operador{:02d}.png".format(i+1), dpi=200)
+  pl.clf()
+  ### Fazendo a reconstrução (rebuild) das trajetórias
+  Frb = np.vstack((Field[0], modelo.simulate(Field[0], n_steps=Field.shape[0]-1)))
+  x1 = Frb[:,0].real
+  x2 = Frb[:,1].real
+  x3 = Frb[:,2].real
+  ### Espaço de fases 1 reconstruido
+  pl.plot(x1, x2, 'r.')
+  pl.xlabel('convecção')
+  pl.ylabel('temperatura horizontal')
+  pl.savefig(DIL+"koopman1{:02d}.png".format(i+1), dpi=200)
+  pl.clf()
+  ### Espaço de fases 2 reconstruido
+  pl.plot(x1, x3, 'r-')
+  pl.xlabel('convecção')
+  pl.ylabel('temperatura vertical')
+  pl.savefig(DIL+"koopman2{:02d}.png".format(i+1), dpi=200)
+  pl.clf()
+
+###### Combinando imagens
+num = 40
+pl.figure(figsize=(8, 8), dpi=128)
+imagem1 = imread(DIL+"efases1{:02d}.png".format(num))
+imagem2 = imread(DIL+"efases2{:02d}.png".format(num))
+imagem3 = imread(DIL+"koopman1{:02d}.png".format(num))
+imagem4 = imread(DIL+"koopman2{:02d}.png".format(num))
+### Espaço de fases 1
+pl.subplot(2, 2, 1)
+pl.axis('off')
+pl.imshow(imagem1)
+pl.title('(A)', y=-0.1)
+### Espaço de fases 2
+pl.subplot(2, 2, 2)
+pl.axis('off')
+pl.imshow(imagem2)
+pl.title('(B)', y=-0.1)
+### Espaço de fases reconstruido 1
+pl.subplot(2, 2, 3)
+pl.axis('off')
+pl.imshow(imagem3)
+pl.title('(C)', y=-0.1)
+### Espaço de fases reconstruido 2
+pl.subplot(2, 2, 4)
+pl.axis('off')
+pl.imshow(imagem4)
+pl.title('(D)', y=-0.1)
+### Salvando
+pl.savefig("prog07/montagem_lorenz.png")
+pl.clf()
+
+###### SISTEMA DE DUFFING
+### Importando dados
+print("\nDuffing:")
+for i in tqdm(range(N)):
+  ### Condições iniciais
+  x0 = np.genfromtxt(DDS+"entrada{:02d}.csv".format(i+1), delimiter=",")
+  ### Trajetórias -> Dados: Data-Drive
+  x = np.genfromtxt(DDS+"saida{:02d}.csv".format(i+1), delimiter=",")
+  teta, omega = x[:,0], x[:,1]
+  ### Plotando o espaço de fases
+  pl.plot(teta, omega, 'b.')
+  pl.title(str(list(np.round(x0,2))))
+  pl.xlabel('teta')
+  pl.ylabel('omega')
+  pl.savefig(DIS+"efases1{:02d}.png".format(i+1), dpi=200)
+  pl.clf()
+  ###### OPERADOR DE KOOPMAN
+  ### Adequando os dados para usarmos no modelo -> Vide [1]
+  Field = np.array([teta, omega]).T
+  ### Determinando o operador (matriz) de Koopman
+  modelo = kp.Koopman()             ### Modelo do método
+  modelo.fit(Field)                 ### Ajustando os dados
+  K = modelo.koopman_matrix.real    ### Determinando a matrix
+  ### Salvando o operador
+  np.savetxt(DKS+"koopman{:02d}.csv".format(i+1), K, delimiter=",")
+  pl.matshow(K)
+  pl.savefig(DIS+"operador{:02d}.png".format(i+1), dpi=200)
+  pl.clf()
+  ### Fazendo a reconstrução (rebuild) das trajetórias
+  Frb = np.vstack((Field[0], modelo.simulate(Field[0], n_steps=Field.shape[0]-1)))
+  x1 = Frb[:,0].real
+  x2 = Frb[:,1].real
+  ### Espaço de fases reconstruido
+  pl.plot(x1, x2, 'r.')
+  pl.xlabel('teta')
+  pl.ylabel('omega')
+  pl.savefig(DIS+"koopman1{:02d}.png".format(i+1), dpi=200)
+  pl.clf()
+
+###### Combinando imagens
+num1 = 6
+num2 = 29
+pl.figure(figsize=(8, 8), dpi=128)
+imagem1 = imread(DIS+"efases1{:02d}.png".format(num1))
+imagem2 = imread(DIS+"efases1{:02d}.png".format(num2))
+imagem3 = imread(DIS+"koopman1{:02d}.png".format(num1))
+imagem4 = imread(DIS+"koopman1{:02d}.png".format(num2))
+### Espaço de fases 1
+pl.subplot(2, 2, 1)
+pl.axis('off')
+pl.imshow(imagem1)
+pl.title('(A)', y=-0.1)
+### Espaço de fases 2
+pl.subplot(2, 2, 2)
+pl.axis('off')
+pl.imshow(imagem2)
+pl.title('(B)', y=-0.1)
+### Espaço de fases reconstruido 1
+pl.subplot(2, 2, 3)
+pl.axis('off')
+pl.imshow(imagem3)
+pl.title('(C)', y=-0.1)
+### Espaço de fases reconstruido 2
+pl.subplot(2, 2, 4)
+pl.axis('off')
+pl.imshow(imagem4)
+pl.title('(D)', y=-0.1)
+### Salvando
+pl.savefig("prog07/montagem_duffing.png")
+pl.clf()
+
+### FIM 
