0aae404d8fe3389d445678cf6b7e533e0c6d67da
davi-ufop <davi.ufop@gmail.com>
2022-01-20 05:25:55 -0300

    reacherv2-dqn

--- artigos/paper01/reinforce/reacherv2/prog06_dlearn.py
+++ artigos/paper01/reinforce/reacherv2/prog06_dlearn.py
@@ -1,1 +1,35 @@
 ### Faça um programa para usarmos deep learning neste sistema
+import gym
+from stable_baselines3 import SAC, DQN
+from stable_baselines3.common.evaluation import evaluate_policy
+
+env = gym.make("Reacher-v2")
+
+# Instantiate the agent, for discrete space use DQN
+model = SAC('MlpPolicy', env, verbose=1)
+# Train the agent
+model.learn(total_timesteps=int(2e5))
+# Save the agent
+model.save("modelo6")
+del model  # delete trained model to demonstrate loading
+
+# Load the trained agent
+# NOTE: if you have loading issue, you can pass `print_system_info=True`
+# to compare the system on which the model was trained vs the current one
+# model = DQN.load("dqn_lunar", env=env, print_system_info=True)
+model = SAC.load("modelo6", env=env)
+
+# Evaluate the agent
+# NOTE: If you use wrappers with your environment that modify rewards,
+#       this will be reflected here. To evaluate with original rewards,
+#       wrap environment in a "Monitor" wrapper before other wrappers.
+mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
+
+# Enjoy trained agent
+obs = env.reset()
+for i in range(1000):
+    action, _states = model.predict(obs, deterministic=True)
+    obs, rewards, dones, info = env.step(action)
+    env.render()
+
+### END
